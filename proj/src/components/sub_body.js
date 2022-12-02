93a417d110d0f055fc409d03292e472d72e19088
bryan zack <bryan.zack@zacklabs.com>
2022-12-02 18:15:28 -0500

    implemented barebones re-search bar

--- proj/src/components/sub_body.js
+++ proj/src/components/sub_body.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './sub_body.css';
-import RegionBox  from './RegionBox.js';
 import PlayerMatch from './PlayerMatch.js';
 import Searchbar from './Searchbar.js';
 
@@ -14,6 +13,7 @@ class sub_body extends React.Component {
       isInvalid: false, 
       apiCode: null,
       serverCode: null,
+      noMatches: false,
       matches: {
         playerData: {},
         gameData: {}
@@ -41,30 +41,46 @@ class sub_body extends React.Component {
             //let array = result["matches"];
             console.log("Connection to Riot api: " + apiResponseCode);
             console.log(result);
+            let noMatches = false;
             if (apiResponseCode === 200) {
               let playerData = {};
               let gameData = {};
               console.log(gameData);
               console.log(result["puuid"]);
-              for (let i = 0; i < result["matches"].length; i++) {
-                gameData[i] = {
-                  "gameMode": result["matches"][i]["info"]["gameMode"],
-                  "gameDuration": result["matches"][i]["info"]["gameDuration"],
-                  "gameEndTimestamp": result["matches"][i]["info"]["gameEndTimestamp"],
-                  "teams": result["matches"][i]["info"]["teams"],
-                  "participants": result["matches"][i]["info"]["participants"]
-                }
-                for (let j = 0; j < 10; j++) {
-                  if (result["puuid"] === result["matches"][i]["info"]["participants"][j]["puuid"]) {
-                    playerData[i] = result["matches"][i]["info"]["participants"][j];
+              console.log(result["matches"]);
+              // if the first match is not found, display no matches found
+              if (result["matches"][0]["status"] != undefined &&
+                result["matches"][0]["status"]["status_code"] === 404) {
+                console.log("NO MATCHES!");
+                noMatches = true; 
+                // if the first match is found, add matches to json if they exist
+              } else {
+                for (let i = 0; i < result["matches"].length; i++) {
+                  if (result["matches"][i]["status"] === undefined) {
+                    gameData[i] = {
+                      "gameMode": result["matches"][i]["info"]["gameMode"],
+                      "gameDuration": result["matches"][i]["info"]["gameDuration"],
+                      "gameEndTimestamp": result["matches"][i]["info"]["gameEndTimestamp"],
+                      "teams": result["matches"][i]["info"]["teams"],
+                      "participants": result["matches"][i]["info"]["participants"]
+                    }
+                    for (let j = 0; j < 10; j++) {
+                      if (result["puuid"] === result["matches"][i]["info"]["participants"][j]["puuid"]) {
+                        playerData[i] = result["matches"][i]["info"]["participants"][j];
+                      }
+                    }
+                  } else {
+                    console.log("no more moatches");
                   }
                 }
               }
+
               console.log(playerData);
               console.log(gameData);
               this.setState({
                 apiCode: 200, 
                 serverCode: 200, 
+                noMatches: noMatches,
                 matches: {
                   "playerData": playerData,
                   "gameData": gameData
@@ -102,8 +118,7 @@ class sub_body extends React.Component {
   render() {
     if (this.state.serverCode === 404 || this.state.serverCode === null) {
       return ( 
-        <Searchbar 
-          className="search" 
+        <Searchbar
           enter={this.handleEnter}
           isInvalid={this.state.isInvalid}
         />
@@ -111,25 +126,38 @@ class sub_body extends React.Component {
     }
     else if (this.state.serverCode === 200) {
       if (this.state.apiCode === 200) {
-        return (
-          <>
-            <Searchbar 
-              className="search" 
-              enter={this.handleEnter}
-              isInvalid={this.state.isInvalid}
-            />
-            <div className="matches">
-              {Object.keys(this.state.matches.playerData).map((key) =>
-                <PlayerMatch
-                  className="match"
-                  key={key}
-                  playerData={this.state.matches.playerData[key]}
-                  gameData={this.state.matches.gameData[key]}
-                  handleClick={this.handleValidEntry}
-                />)}
-            </div>
-          </>
-        )
+        if (!this.state.noMatches) {
+          return (
+            <>
+              <Searchbar 
+                enter={this.handleEnter}
+                isInvalid={this.state.isInvalid}
+              />
+              <div className="matches">
+                {Object.keys(this.state.matches.playerData).map((key) =>
+                  <PlayerMatch
+                    className="match"
+                    key={key}
+                    playerData={this.state.matches.playerData[key]}
+                    gameData={this.state.matches.gameData[key]}
+                    handleClick={this.handleValidEntry}
+                  />)}
+              </div>
+            </>
+          )
+        } else {
+          return (
+            <>
+              <Searchbar 
+                enter={this.handleEnter}
+                isInvalid={this.state.isInvalid}
+              />
+              <div className="matches">
+                <h1>No matches found...</h1>
+              </div>
+            </>
+          )
+        }
       }
       else if (this.state.apiCode === 404) {
         return (
